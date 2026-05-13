5df745457e985432e14a527efaa01a65629d1145
MaximoMM <maximomm@gmail.com>
2026-05-13 18:24:22 -0300

    Ajuste archivo server.js, renombre rutas y agregue script dev

---
+++ routes/serviciosRoutes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { getServicios, getServicioById } = require('../controllers/serviciosController');
+
+const router = Router();
+
+router.get('/', getServicios);
+
+router.get('/:id', getServicioById);
+
+module.exports = router;
