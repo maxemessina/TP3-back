83143bfab2694e77d13d40107d368f5bc10b304e
Lucas Rojas <Lucasroket203@gmail.com>
2026-05-13 19:50:47 -0300

    creo y conecto los datos

--- models/server.js
+++ models/server.js
@@ -13,7 +13,8 @@ class Server {
             servicios: '/api/servicios',
             equipo:    '/api/equipo',
             usuarios:  '/api/usuarios',
-            auth:      '/api/auth'
+            auth:      '/api/auth',
+            pedido:    '/api/pedido'
         };
 
         this.middlewares();
@@ -30,6 +31,7 @@ class Server {
     routes() {
         // archivos de rutas tienen que ir siendo creados
         this.app.use(this.paths.index, require('../routes/indexRoute'));
+        this.app.use(this.paths.pedido, require('../routes/pedidosroutes'));
         // this.app.use(this.paths.servicios, require('../routes/serviciosRoutes'));
         // this.app.use(this.paths.equipo,    require('../routes/equipoRoutes'));
         // this.app.use(this.paths.usuarios,  require('../routes/usuariosRoutes'));
