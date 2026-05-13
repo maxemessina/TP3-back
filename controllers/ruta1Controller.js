cdea12c4e11e5291ef9301ef207b8177051f5f37
Máximo <messinamaximo@gmail.com>
2026-05-13 12:48:28 -0300

    Primera prueba con index

--- controllers/ruta1Controller.js
+++
@@ -1,1 +0,0 @@
-// ./controllers: OPCIONAL: No es obligatorio, pero por cada ruta nueva que le agregamos al proyecto, se puede separar la lógica en archivos diferentes, y estos se deben guardar acá. Es una buena práctica agregar ‘Controller’ al final del nombre del archivo, por ejemplo: ‘serviciosController’.
--- models/server.js
+++ models/server.js
@@ -9,10 +9,11 @@ class Server {
 
         // nombres de las rutas (endpoints)
         this.paths = {
-            servicios: '/api/servicios',
-            equipo:    '/api/equipo',
-            usuarios:  '/api/usuarios',
-            auth:      '/api/auth'
+            index:     '/api/index',
+            // servicios: '/api/servicios',
+            // equipo:    '/api/equipo',
+            // usuarios:  '/api/usuarios',
+            // auth:      '/api/auth'
         };
 
         this.middlewares();
@@ -28,10 +29,11 @@ class Server {
 
     routes() {
         // archivos de rutas tienen que ir siendo creados
-        this.app.use(this.paths.servicios, require('../routes/serviciosRoutes'));
-        this.app.use(this.paths.equipo,    require('../routes/equipoRoutes'));
-        this.app.use(this.paths.usuarios,  require('../routes/usuariosRoutes'));
-        this.app.use(this.paths.auth,      require('../routes/authRoutes'));
+        this.app.use(this.paths.index, require('../routes/indexRoute'));
+        // this.app.use(this.paths.servicios, require('../routes/serviciosRoutes'));
+        // this.app.use(this.paths.equipo,    require('../routes/equipoRoutes'));
+        // this.app.use(this.paths.usuarios,  require('../routes/usuariosRoutes'));
+        // this.app.use(this.paths.auth,      require('../routes/authRoutes'));
     }
 
     listen() {
