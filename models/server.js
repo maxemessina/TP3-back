34e37711daad1da623a6538000ecf6c4730996d8
Lucas Rojas <Lucasroket203@gmail.com>
2026-05-13 19:58:34 -0300

    arreglo el server.js

--- models/server.js
+++ models/server.js
@@ -31,11 +31,11 @@ class Server {
     routes() {
         // archivos de rutas tienen que ir siendo creados
         this.app.use(this.paths.index, require('../routes/indexRoute'));
-        this.app.use(this.paths.pedido, require('../routes/pedidosroutes'));
         // this.app.use(this.paths.servicios, require('../routes/serviciosRoutes'));
         // this.app.use(this.paths.equipo,    require('../routes/equipoRoutes'));
         // this.app.use(this.paths.usuarios,  require('../routes/usuariosRoutes'));
         // this.app.use(this.paths.auth,      require('../routes/authRoutes'));
+        this.app.use(this.paths.pedido, require('../routes/pedidosroutes'));
     }
 
     listen() {
