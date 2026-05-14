226fe2f3d183631c01ac50a9e889f646dba5e9e7
Valenguerrieri <143465626+Valenguerrieri@users.noreply.github.com>
2026-05-13 22:52:16 -0300

    corrigo para poder renderizar

--- models/faq.js
+++ models/faq.js
@@ -1,22 +1,15 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-
 class FAQModel {
+  constructor() {
+    this.path = path.join(__dirname, '../data/faq.json');
+  }
 
-
-    constructor() {
-        this.path = path.join(__dirname, '../data/faq.json');
-    }
-
-
-    async leerTodos() {
-
-
-        const data = await fs.readFile(this.path, 'utf-8');
-
-
-        return JSON.parse(data);
-    }
-
+  async leerTodos() {
+    const data = await fs.readFile(this.path, 'utf-8');
+    return JSON.parse(data);
+  }
 }
+
+module.exports = FAQModel;
--- routes/faqRoutes.js
+++ routes/faqRoutes.js
@@ -3,8 +3,7 @@ const router = Router();
 
 const { obtenerFAQ } = require('../controllers/faqController');
 
+// Ruta base: /api/faq
 router.get('/', obtenerFAQ);
 
 module.exports = router;
-
-
